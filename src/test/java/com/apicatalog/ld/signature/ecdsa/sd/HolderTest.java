91cff19ab46a5dda9607e66ddbf875624b05bd82
Filip <filip26@gmail.com>
2024-02-25 04:22:10 +0100

    Improve tests

---
+++ src/test/java/com/apicatalog/ld/signature/ecdsa/sd/HolderTest.java
@@ -0,0 +1,47 @@
+package com.apicatalog.ld.signature.ecdsa.sd;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import com.apicatalog.jsonld.json.JsonLdComparison;
+import com.apicatalog.vc.holder.Holder;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+
+public class HolderTest {
+
+    static final Holder HOLDER = Holder.with(new ECDSASD2023Suite());
+
+    @Test
+    void testDerive() throws IOException {
+
+        JsonObject sdoc = fetchResource("tv-01-sdoc.jsonld");
+        JsonObject ddoc = fetchResource("tv-01-ddoc.jsonld");
+
+        JsonObject derived = HOLDER.derive(sdoc, Arrays.asList("/credentialSubject/boards/0", "/credentialSubject/boards/1"));
+
+        assertNotNull(derived);
+
+        if (!JsonLdComparison.equals(ddoc, derived)) {
+            System.out.println("Expected:");
+            System.out.println(IssuerTest.write(ddoc));
+            System.out.println("Actual:");
+            System.out.println(IssuerTest.write(derived));
+            fail("Expected does not match actual.");
+        }
+
+    }
+
+    JsonObject fetchResource(String name) throws IOException {
+        try (InputStream is = getClass().getResourceAsStream(name)) {
+            return Json.createReader(is).readObject();
+        }
+    }
+}
--- src/test/java/com/apicatalog/ld/signature/ecdsa/sd/VerifierTest.java
+++ src/test/java/com/apicatalog/ld/signature/ecdsa/sd/VerifierTest.java
@@ -1,5 +1,7 @@
 package com.apicatalog.ld.signature.ecdsa.sd;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
@@ -7,7 +9,9 @@ import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
 
+import com.apicatalog.ld.DocumentError;
 import com.apicatalog.ld.signature.VerificationError;
+import com.apicatalog.vc.Verifiable;
 import com.apicatalog.vc.verifier.Verifier;
 
 import jakarta.json.Json;
@@ -15,12 +19,24 @@ import jakarta.json.JsonObject;
 
 public class VerifierTest {
 
+    static final Verifier VERIFIER = Verifier.with(new ECDSASD2023Suite());
+    
     @Test
     void testVerifyBase() throws IOException  {
 
         JsonObject sdoc = fetchResource("tv-01-sdoc.jsonld");
 
-        assertThrows(VerificationError.class, () -> Verifier.with(new ECDSASD2023Suite()).verify(sdoc));        
+        assertThrows(VerificationError.class, () -> VERIFIER.verify(sdoc));        
+    }
+    
+    @Test
+    void testVerifyDerived() throws IOException, VerificationError, DocumentError {
+
+        JsonObject ddoc = fetchResource("tv-01-ddoc.jsonld");
+
+        Verifiable verifiable = VERIFIER.verify(ddoc);
+        
+        assertNotNull(verifiable);
     }
 
     JsonObject fetchResource(String name) throws IOException {
